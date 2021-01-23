13f0c297a4f0f8653b39b0c4ed7a61d5899fa133
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-23 12:55:43 -0300

    :sparkles: display products in cart, send cart data to backend, finish cart

--- src/ui/views/Cart/CartView/index.js
+++ src/ui/views/Cart/CartView/index.js
@@ -1,10 +1,73 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {SafeAreaView, ScrollView} from 'react-native';
+import {Appbar, List, IconButton, Button, Snackbar} from 'react-native-paper';
+import {CartContext} from '../../../providers/CartProvider';
+import {NumberService} from '../../../../data/services/NumberService';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import {
+  cartClear,
+  cartRemoveProduct,
+} from '../../../../data/actions/CartActions';
+import {ApiService} from '../../../../data/services/ApiService';
 
 export default function CartView(props) {
+  const [isMessageVisible, setIsMessageVisible] = useState(false);
+  const [{products}, cartDispatcher] = useContext(CartContext);
+
+  function removeFromCart(item) {
+    cartDispatcher(cartRemoveProduct(item));
+  }
+
+  function finishCart() {
+    ApiService.post('pedidos', {pedido: products});
+    cartDispatcher(cartClear());
+    setIsMessageVisible(true);
+  }
+
+  function getTotal() {
+    const value = products
+      .map((item) => item.product.price)
+      .reduce((previous, current) => previous + current, 0);
+
+    return NumberService.currency(value);
+  }
+
   return (
-    <SafeAreaView>
-      <Text>Carrinho</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <Appbar.Header>
+        <Appbar.Content title="Carrinho" />
+      </Appbar.Header>
+      <ScrollView>
+        <List.Section>
+          {products.map((item, index) => (
+            <List.Item
+              key={index}
+              title={item.product.name}
+              description={NumberService.currency(item.product.price)}
+              right={() => (
+                <IconButton
+                  onPress={() => removeFromCart(item)}
+                  icon={({size, color}) => (
+                    <FontAwesomeIcon icon={faTimes} size={size} color={color} />
+                  )}
+                />
+              )}
+            />
+          ))}
+          {products.length !== 0 && (
+            <Button mode="contained" onPress={finishCart}>
+              Finalizar Compra {getTotal()}
+            </Button>
+          )}
+        </List.Section>
+      </ScrollView>
+      <Snackbar
+        visible={isMessageVisible}
+        onDismiss={() => setIsMessageVisible(false)}
+        duration={3000}>
+        Seu pedido foi enviado!
+      </Snackbar>
     </SafeAreaView>
   );
 }
