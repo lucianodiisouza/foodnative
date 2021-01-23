ad5ffb85794f5910b9b2a6a22e24554a382c51cd
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-22 22:31:26 -0300

    applying this commit you will implement providers from Cart and Product to you application and vinculate this inside index.js file

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {CartProvider, ProductProvider} from './src/ui/providers';
 
 const theme = {
   ...DefaultTheme,
   dark: true,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#069',
+    primary: '#a91f1f',
   },
 };
 
 export default function Main() {
-  return;
-  <PaperProvider theme={theme}>
-    <App />;
-  </PaperProvider>;
+  return (
+    <PaperProvider theme={theme}>
+      <ProductProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductProvider>
+    </PaperProvider>
+  );
 }
 
 AppRegistry.registerComponent(appName, () => Main);
---
+++ src/ui/providers/index.js
@@ -0,0 +1,2 @@
+export {default as ProductProvider} from './ProductProvider';
+export {default as CartProvider} from './CartProvider';
